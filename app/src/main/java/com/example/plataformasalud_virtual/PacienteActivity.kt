a450f9fe1d830fbf08c03ac6d475107b6d640275
Cano7 <148022578+Cano7@users.noreply.github.com>
2025-09-10 20:23:38 -0600

    Nuevas pantallas y menú funcional

---
+++ app/src/main/java/com/example/plataformasalud_virtual/PacienteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.plataformasalud_virtual
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PacienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nav_paciente)
+    }
+}
