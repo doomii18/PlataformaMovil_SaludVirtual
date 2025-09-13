a8f4670f4ea5eeee6c243230246c35c5482a2816
Sury Massiel Jaen Pérez <jaensury927@gmail.com>
2025-09-13 15:03:14 -0600

    Commint #6
    Validación del Login, corrección de Layouts

--- app/src/main/java/com/example/plataformasalud_virtual/LoginActivity.kt
+++ app/src/main/java/com/example/plataformasalud_virtual/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.plataformasalud_virtual
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -15,22 +17,41 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        // Referencias a los campos
+        val usuario = findViewById<EditText>(R.id.editTextText)
+        val password = findViewById<EditText>(R.id.editTextTextPassword)
+        val btnAcceder = findViewById<Button>(R.id.button)
+        val registro = findViewById<TextView>(R.id.registro)
 
+        // Acción al hacer clic en "Acceder"
+        btnAcceder.setOnClickListener {
+            val userText = usuario.text.toString().trim()
+            val passText = password.text.toString().trim()
 
-        // Referencia al botón de acceder
-        val accederButton: Button = findViewById(R.id.button) // Asegúrate de que el ID sea correcto
-        accederButton.setOnClickListener {
-            // Navegar a MenudeGestion
-            val intent = Intent(this, MenudeGestion::class.java)
-            startActivity(intent)
+            when {
+                userText.isEmpty() -> {
+                    usuario.error = "El usuario es obligatorio"
+                }
+                passText.isEmpty() -> {
+                    password.error = "La contraseña es obligatoria"
+                }
+                passText.length < 8 -> {
+                    password.error = "La contraseña debe tener al menos 8 caracteres"
+                }
+                else -> {
+                    Toast.makeText(this, "Validación correcta", Toast.LENGTH_SHORT).show()
+
+                    // Si pasa la validación, navegar a MenudeGestion
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                }
+            }
         }
 
-        // Referencia al texto de registro
-        val registro: TextView = findViewById(R.id.registro)
+        // Acción al hacer clic en "Registrarse"
         registro.setOnClickListener {
             val intent = Intent(this, RegistrarseActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
